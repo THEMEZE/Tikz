ecebe2f67d022ff3630a032524b90ae5b08b08c3
THEMEZE <guillaume.themeze@universite-paris-saclay.fr>
2023-12-24 20:43:07 +0100

    commit 1

---
+++ perpective-telecharge.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Vérifier si tlmgr est installé
+if ! command -v tlmgr &> /dev/null; then
+    echo "TeX Live n'est pas installé sur votre système. Veuillez installer TeX Live."
+    exit 1
+fi
+
+# Installer le package perspective
+tlmgr install perspective
+
+# Vérifier si l'installation a réussi
+if [ $? -eq 0 ]; then
+    echo "Le package perspective a été installé avec succès."
+else
+    echo "Erreur lors de l'installation du package perspective."
+    exit 1
+fi
+
+pdflatex  Perspective.tex
+
